124e9f1859d08862215c2573a455514c74af6a4b
John Grieve <jonathanrgrieve@gmail.com>
2022-05-11 12:56:30 -0500

    adds registrations controller

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,11 @@
+class RegistrationsController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      render json: @user
+    else
+      render json: @user.errors
+    end
+  end
+end
