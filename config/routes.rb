9ff6923889cfb35db56a1e99ca3e7b746b434a21
John Grieve <jonathanrgrieve@gmail.com>
2022-05-11 12:37:59 -0500

    adds update and destroy routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "/login", to: "sessions#create"
-  get "/logout", to: "sessions#logout"
-  get "/signup", to: "registrations#create"
+  post "/sessions", to: "sessions#create"
+  post "/users", to: "sessions#logout"
+  post "/users", to: "users#create"
+
+  get "/players", to: "players#index"
+  get "/players", to: "players#show"
+  patch "/players/:id", to: "players#update"
+  delete "/players/:id", to: "players#destroy"
 end
